03e9b9c10ce21741a1cc5e71f0162cfd796d8958
Jakub Stachurski <jakub@wilkuu.xyz>
2023-12-04 20:36:56 +0100

    Normalize and correct stuff

--- Week3_4/2_network_effects.py
+++ Week3_4/2_network_effects.py
@@ -51,7 +51,7 @@ def get_all_song_timelines(data: List[ParsedFile]):
     return (timelines,titles, dates) 
 
 
-def get_all_song_derivatives(timelines: Dict[str, Dict[datetime, Any]]) -> Dict[str, Dict[datetime, float]]: 
+def get_all_song_derivatives(timelines: Dict[str, Dict[datetime, Any]]) -> Dict[str, Dict[datetime, float | None]]: 
     derivatives = {}  
     for id, timeline in timelines.items(): 
         derivatives[id] = {} 
@@ -78,7 +78,7 @@ def get_all_song_derivatives(timelines: Dict[str, Dict[datetime, Any]]) -> Dict[
 def max_viewcounts(timeline: Dict[datetime, Any]) -> int: 
     return max([int(x["statistics"]["viewCount"]) if x is not None else 0 for x in timeline.values()])
 
-def average_daily(derivative: Dict[datetime, float]) -> float: 
+def average_daily(derivative: Dict[datetime, float | None]) -> float: 
     N = 0
     sum = 0
 
@@ -89,7 +89,7 @@ def average_daily(derivative: Dict[datetime, float]) -> float:
 
     return sum / N  
 
-def median_daily(derivative: Dict[datetime, float]) -> np.floating[Any]:
+def median_daily(derivative: Dict[datetime, float | None]) -> np.floating[Any]:
     valid_nums: List[float] = [] 
     
     for d in derivative.values(): 
@@ -98,8 +98,41 @@ def median_daily(derivative: Dict[datetime, float]) -> np.floating[Any]:
 
     return np.median(valid_nums)
 
-        
-        
+
+def extract_timeline_viewcounts(timeline: Dict[datetime,Any]) -> Dict[datetime,int]:  
+    """
+    Extracts the viewcount out of the timeline 
+    Returns a Dict of the same structure, but with only the viewcount instead of the whole JSON object 
+    """
+    return dict([(x,y["statistics"]["viewCount"]) if y is not None else (x,None) for x,y in timeline.items()])
+
+def extract_all_viewcounts(timelines: Dict[str,Dict[datetime,Any]]) -> Dict[str,Dict[datetime,int]]:
+    """
+    Call the extract_timeline_viewcounts on a whole collection of timelines 
+    Returns a Dict of the same structure, but with only the viewcount instead of the whole JSON object 
+    """
+    out = {} 
+    for id, timeline in timelines.items():
+        out[id] = extract_timeline_viewcounts(timeline)
+    return out 
+
+
+def normalize_all():
+    pass 
+    # To be implemented
+
+def normalize(inp: Dict[Any,Any]) -> Dict[Any,float | None]:
+    """
+    Normalize a dictionary of id's and values so that all of the values sum up to one 
+    """ 
+    total = sum([x if x is not None else 0 for x in inp.values()])
+    return dict([(k, v/total) if v is not None else (k,None) for k,v in inp.items()])
+
+
+def normalize_arr(inp: List[float]) -> List[float]:
+    s = sum(inp)
+    return [x/s for x in inp]
+    
 
 if __name__ == "__main__": 
     print("Week 3/4 Assignment 2")
@@ -124,9 +157,8 @@ if __name__ == "__main__":
     EXCL_BOT = 0
     EXCL_TOP = 20 
 
-    graph_median_change_per_day = [median_daily(derivative) for derivative in derivatives.values()]
-    graph_max_viewcount = [max_viewcounts(timeline) for timeline in timelines.values()]
-
+    graph_median_change_per_day = [float(median_daily(derivative)) for derivative in derivatives.values()]
+    graph_max_viewcount = normalize_arr([max_viewcounts(timeline) for timeline in timelines.values()])
     ax_scatter.scatter(graph_median_change_per_day,graph_max_viewcount)
 
     for id,derivative in list(derivatives.items())[EXCL_BOT:len(derivatives) - EXCL_TOP]: 
--- Week3_4/Excercise_2.md
+++ Week3_4/Excercise_2.md
@@ -6,10 +6,12 @@
 When looking at the daily views (dV/dt) 
 we can see that the daily views seem to stay around the same position relative 
 to the songs more and less viewed by them. This implies that the daily viewership is related to the 
-viewership of the previous day.
+total amount of views. 
 The songs daily viewership seems to stabilize somewhere under 0.4e7 viewers per day as they get less popular over time
+Either because it gets old, being surpassed by new music or people overplaying it.
 
-This could be called a Networking effect, since the popular songs seem to stay popular relative to other songs. 
+Additionally when plotting 
+
 
 ## Compared to the regular network effect model
 
@@ -28,9 +30,13 @@ This could be called a Networking effect, since the popular songs seem to stay p
 
 ##  Metrics
 - Intrinsic interest r(x): 
-    Free time for listening needed to listen to a particular song, for each user(/view) 
-- Network benefit f(z) is the popularity of the song or the expectation thereof. 
-    Higher viewed songs tend to grow quicker.
+    Amount of total time spent on youtube listening to songs when the song is played x times 
+    
+- Network benefit f(z):
+    The popularity of the song or the expectation thereof. 
+    This can be influenced by views, subscribes, comments and like-dislike ratios
+    In addition, because of the youtube algorithm, popular songs get recommended more 
+
 - Reservation price p*: 
     The amount of time people have to listen to songs, fluctuates day from day
 
@@ -40,7 +46,9 @@ This mostly depends on taste, but also might be dependent on the likes and disli
 ### Another metrics that might affect f(z)
 The popularity of the artist. Some artists have multiple songs in the top 100, like Drake etc. 
 This is because people who listen to song made my an artist might listen to the artist's other songs 
-because they liked the first song.  
+because they liked the first song.
+The youtube algoritm and it's recommendations
+
 
 ### Another metrics that might affect p*
 Day of the week. People have more time on the weekends.
