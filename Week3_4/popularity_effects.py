8ebac4fcba9047e8cef78fe2e84c7aced46bd2d2
kallivdh <kallivdh@gmail.com>
2023-12-07 14:38:32 +0100

    yt top songs with views and spotify top songs

--- Week3_4/popularity_effects.py
+++ Week3_4/popularity_effects.py
@@ -6,19 +6,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-DAY_OFFSET = 0;
-NUMBER_OF_DAYS = 7;
-BINS = 100;
+DAY_OFFSET = 0
+NUMBER_OF_DAYS = 7
+BINS = 50
+AMOUNT_SHOWN_IN_TOP = 15
 
-
-# Get first songs ids of first entry
+# Filling a set of unique songs ids
 res = asyncio.run(gen_db("Week3_4/youtube_top100"))
 song_ids = set()
 for i in res:
     song_ids.add(i.json[random.randrange(len(i.json))]["id"])
 
-# loops through every json file in folder and searches for the given song_id. Lastly returning the array of the matching entries.
 def get_all_song_data_points(song_id):
+    """
+    Loops through every json file in folder and searches for the given song_id. Lastly returning the array of the matching entries.
+    """
     song_data_point_list = {}
     for data_point in res:
         for song in data_point.json:
@@ -26,35 +28,71 @@ def get_all_song_data_points(song_id):
                 song_data_point_list[datetime_from_filename(data_point.filename)] = song
     return song_data_point_list
 
-figure, axis = plt.subplots(1, 2)
-total_views = []
+figure, axis = plt.subplots(2, 2)
+total_song_data = []
 
 for song_id in song_ids:
     song_data = get_all_song_data_points(song_id)
-    # single usage of function for mapping
+    # single usage function for mapping
     def map_song_views(sorted_song):
         view_count = int(sorted_song[1]["statistics"]["viewCount"])
-        return (sorted_song[0], view_count)
+        name = sorted_song[1]["snippet"]["title"]
+        return (sorted_song[0], view_count, name)
 
     mapped_song_data = list(map(map_song_views, song_data.items()))
-    data_points, views = list(zip(*mapped_song_data))
-    
-    total_views.extend(views[DAY_OFFSET:NUMBER_OF_DAYS-1])
+    # data_points, views = list(zip(*mapped_song_data))
+    total_song_data.append(mapped_song_data[DAY_OFFSET:NUMBER_OF_DAYS-1])
 
-hist, edges = np.histogram(total_views, bins=BINS)
-axis[0].scatter(edges[:-1], hist)
+total_song_data_squashed = []
+for song in total_song_data:
+    for data_point in song:
+        date = data_point[0]
+        views = data_point[1]
+        total_song_data_squashed.append((date,views))
 
-axis[0].set_title('Amount of view over period 7 days starting from baseline')
-axis[0].set_xlabel('Views')
-axis[0].set_ylabel('Amount of songs')
+data_points, views = list(zip(*total_song_data_squashed))
+hist, edges = np.histogram(views, bins=BINS)
+axis[0,0].scatter(edges[:-1], hist)
+axis[0,0].set_title('Amount of view over period 7 days starting from baseline')
+axis[0,0].set_xlabel('Views')
+axis[0,0].set_ylabel('Amount of songs')
 
 # Get the centers of the bins for the scatter plot
 bin_centers = (edges[:-1] + edges[1:]) / 2.0
-
 # Create a log-log scatter plot
-axis[1].loglog(bin_centers, hist, 'o')
+axis[0,1].loglog(bin_centers, hist, 'o')
+axis[0,1].set_title('Same data but logarithmic view')
+axis[0,1].set_xlabel('Rank')
+axis[0,1].set_ylabel('Log views')
 
-axis[1].set_title('Same data but logarithmic view')
-axis[1].set_xlabel('Rank')
-axis[1].set_ylabel('Log views')
+# First sort the data points chronologically
+sorted_total_song_data = []
+for song_data in total_song_data:
+    sorted_total_song_data.append(sorted(song_data, key=lambda x: x[0]))
+
+# Then sort based on the last data point the amount of views
+sorted_total_song_data = sorted(sorted_total_song_data, key=lambda x: x[-1][1], reverse=True)
+i = 0
+# Plotting top songs
+for song_data in sorted_total_song_data:
+    data_points, views, name = list(zip(*song_data))
+    axis[1,0].plot(data_points, views, label = name[0])
+    i += 1;
+    if i == AMOUNT_SHOWN_IN_TOP: break
+axis[1,0].legend()
+
+# Getting spotify data
+res2 = asyncio.run(gen_db("Week3_4/spotify_top100"))
+spotify_data = {}
+for data_point in res2:
+    spotify_data[datetime_from_filename(data_point.filename)] = data_point.json['tracks']['items']
+
+# Getting spotify data from last data point
+rank_data_point = sorted_total_song_data[0][-1][0]
+ranking_data = spotify_data[rank_data_point]
+print(rank_data_point)
+# Printing the top 10
+for i in range(AMOUNT_SHOWN_IN_TOP):
+    artist_names = list(map(lambda artist: artist['name'],ranking_data[i]['track']['artists']))
+    print(artist_names, "-", ranking_data[i]['track']['name'])
 plt.show()
