be8dcddbffb49f5e64de82cc3b1b6fa0fa077465
Jakub Stachurski <jakub@wilkuu.xyz>
2023-12-07 12:51:17 +0100

    Comments and stuff

--- Week3_4/yt_api_crawler.py
+++ Week3_4/yt_api_crawler.py
@@ -7,12 +7,14 @@ import googleapiclient.discovery
 import googleapiclient.errors
 import json 
 import time 
-from datetime import datetime, timedelta 
 
 api_service_name = "youtube"
 api_version = "v3"
 
 def youtube_authenticate(token):
+    """
+    Creates a authenticated Youtube API handle 
+    """
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -20,33 +22,39 @@ def youtube_authenticate(token):
     return googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=token)
 
-def query(yt,batchsize=25,page_token = None, regionCode="US",language="en"):
+def query(yt,q,batchsize=25,page_token = None, regionCode="US",language="en"):
+    """
+    Pulls a batch of top videos from the youtube api, this is completed in `batchsize + 1` requests
+    Returns a list of video resource items 
+    """
     request = yt.search().list(
-        part="id",
-        maxResults=batchsize,
-        q="music",
+        part="id", # Only get the id, we'll get the rest in the next request 
+        maxResults=batchsize, 
+        q=q, # Query using the word music to ensure we only get music videos 
         regionCode=regionCode,
         relevanceLanguage=language,
-        type="video",
-        order="viewCount",
-        videoCategoryId="10",
-        videoDuration="short",
-        videoType="any",
+        type="video", # Ensure that we get videos instead of channels 
+        order="viewCount", # Get the top viewed videos 
+        videoCategoryId="10", # Type: Music 
+        videoDuration="short", # Avoid playlists/compilations 
+        videoType="movie", # Avoid shorts  
         pageToken="" if page_token is None else page_token,
     )
-    
+
+    # Get all the id json and parse it  
     ids = request.execute()
-    
     idlist = [id["id"]["videoId"] for id in ids["items"]]
-    
+   
+    # See if we get the next page token  
     page_token = None 
     try: 
         page_token = ids["nextPageToken"]
     except: 
         pass
     
+
+    # Fetch all the video info 
     items = []
-    
     for id in idlist:  
         request = yt.videos().list(
             part="id,statistics,snippet",
@@ -55,22 +63,24 @@ def query(yt,batchsize=25,page_token = None, regionCode="US",language="en"):
         )
         items.extend(request.execute()["items"])
 
+    # Return the items and the token for the next page 
     return items, page_token
 
 
-def get_yt_data(token, batchsize=5, number=20, interval=2.0, region="US", language="en"):
+def get_yt_data(token, batchsize=5, number=20, interval=2.0, region="US", language="en", q="music"):
+    """
+    A method that gathers a top `number` of videos in the given regioni and in a given language. 
+    Returns a list of json objects which are the video resource items 
+    """
     yt = youtube_authenticate(token)
-
+    
+    # Repeadetly call query to get the desired amount of videos 
     dataset = []
-    before = datetime.utcnow()
-    after = before - timedelta(365,0,0)
-
     page_token = None
-    before = after 
-    after  = before - timedelta(365,0,0) 
     while len(dataset) < int(number):
         print(f"Pulling from api with page_token={page_token}")
         q, page_token = query(yt,
+                              q,
                               batchsize=min(batchsize, 50),
                               page_token=page_token,
                               regionCode=region,
@@ -90,6 +100,7 @@ def get_yt_data(token, batchsize=5, number=20, interval=2.0, region="US", langua
     return dataset
 
 def write_yt_data(filename, data):
+    """Writes the data from youtube in a file.""" 
     with open(filename, "w") as f: 
         f.write(json.dumps(data)) 
     f.close()
@@ -98,7 +109,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
                         prog='YT_API_Crawler',
                         description='Crawls the Youtube recommends',
-                        epilog='"Huhg huh" -Minecraft Villager') # Idk what to put there... 
+                        epilog='All your base are belong to us.') # Idk what to put there... 
 
     parser.add_argument("YT_API_TOKEN", help="The Youtube API Token")
     parser.add_argument("filename", help="Output filename")
