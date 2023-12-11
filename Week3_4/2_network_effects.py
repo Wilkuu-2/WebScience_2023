b0358195fb435414f286f2242b2439c3c78bf81f
Jakub Stachurski <jakub@wilkuu.xyz>
2023-12-11 12:11:46 +0100

    Cumulative plots

--- Week3_4/2_network_effects.py
+++ Week3_4/2_network_effects.py
@@ -90,6 +90,32 @@ def extract_all_viewcounts(timelines: Dict[str,Dict[datetime,Any]]) -> Dict[str,
         out[id] = extract_timeline_viewcounts(timeline)
     return out 
 
+
+def daily_views(timelines: Dict[str, Dict[datetime, int]]) -> Dict[str, Dict[datetime, float | None]]: 
+    dailies = {}  
+    for id, timeline in timelines.items(): 
+        dailies[id] = {} 
+        prev = 0 
+        prev_date: datetime | None = None 
+        for t, d in timeline.items(): 
+            if d is not None: 
+                vc = d 
+                if prev_date == None : 
+                    dailies[id][t] = None 
+                else: 
+                    days = (t - prev_date).total_seconds() / 3600.0 / 24.0
+                    # print(f"{t} - {prev_date} = {t-prev_date} : {days}")
+                    dailies[id][t] = (vc - prev) / days 
+
+                prev_date = t; 
+                prev = vc
+            else:
+                dailies[id][t] = None 
+
+    return dailies 
+
+
+
 if __name__ == "__main__": 
     print("Week 3/4 Assignment 2")
     data = asyncio.run(gen_db("Week3_4/youtube_top100"))
@@ -99,43 +125,55 @@ if __name__ == "__main__":
     print(f"[INFO] First file: {data[0].filename}")
     print(f"[INFO] date: {data[0].parse_filename()}")
     print(f"[INFO] data: CORRECT")
-    fig, (ax_daily) = plt.subplots(1)
+    fig, (ax_cum, ax_daily)  = plt.subplots(2)
 
+    plot_limitsC = [999999, -999999]
     plot_limitsD = [999999, -999999]
 
     timelines,titles,dates = get_all_song_timelines(data)
     timelines = dict(sorted(list(timelines.items()), key=lambda x: max_viewcounts(x[1]))) # Sort by total view count 
     viewcounts = extract_all_viewcounts(timelines)
+    daily = daily_views(viewcounts)
 
     # Get the data, filtered by the top 100 songs 
     EXCL_BOT = 20 
     EXCL_TOP = 0 
-    
-    plot_data = list(viewcounts.items())[EXCL_BOT:len(viewcounts) - EXCL_TOP]
-    plot_len = len(plot_data)
+        
+    plot_cumulative = list(viewcounts.items())[EXCL_BOT:len(viewcounts) - EXCL_TOP] 
+    plot_count = len(plot_cumulative)
+    plot_len = len(plot_cumulative)
     plot_progress =0
+        
 
     print("[INFO]: Plotting: ")
-    for id,vc in plot_data: 
+    for id,vc in plot_cumulative: 
         plot_progress +=1
         print(f"[PROGRESS]: {plot_progress}/{plot_len}", end='\r')
         graph_date = []
         graph_viewcounts = []
+        graph_daily = [] 
         title = titles[id]
         for t, d in vc.items(): 
             if t is not None and d is not None:
                 graph_date.append(t) 
                 graph_viewcounts.append(d)
+                graph_daily.append(daily[id][t])
             
+        plot_limitsC[0] = min(plot_limitsC[0], min(graph_viewcounts))
+        plot_limitsC[1] = max(plot_limitsC[1], max(graph_viewcounts)) 
         plot_limitsD[0] = min(plot_limitsD[0], min(graph_viewcounts))
         plot_limitsD[1] = max(plot_limitsD[1], max(graph_viewcounts)) 
 
-        ax_daily.plot(graph_date, graph_viewcounts, label=title)
+        ax_cum.plot(graph_date, graph_viewcounts, label=title)
+        ax_daily.plot(graph_date, graph_daily, label=title)
+
+    ax_cum.set_xlabel("Date") 
+    ax_cum.set_ylabel("Total views")
+    ax_cum.set_title(f"Total views per day of the top {plot_len} songs on youtube")
 
     ax_daily.set_xlabel("Date") 
-    ax_daily.set_ylabel("Total views")
-    ax_daily.set_title("Total views per day of the top 100 songs on youtube")
-
+    ax_daily.set_ylabel("Daily views")
+    ax_daily.set_title(f"Daily views for the top {plot_len} songs on youtube")
         
     print("[PROGRESS]: DONE             ")
     save_path = "./Figure_2_1.png"
